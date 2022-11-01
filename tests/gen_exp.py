f2bc05c4ffe69acdf111c7092209fe3038083247
Robert Lindgren <rob.lindgren@gatech.edu>
2022-11-01 16:32:23 -0400

    Add experiment generator and LSTM test script
    Add two files, gen_exp.py and test_lstm_exp.py, plus a readme.
    
    gen_exp.py
    - Creates train and test data for continuous recognition n-back experiments
    - The data are bit-arrays that represent randomly selected integers
    - The bit-arrays are ordered to match randomly generated target arrays of 0s (unseen) and 1s (seen)
    
    test_lstm_exp.py
    - A script to create an LSTM model and test it on continuous recognition n-back experiments

---
+++ tests/gen_exp.py
@@ -0,0 +1,131 @@
+import numpy as np
+import random
+from math import floor
+
+
+class expBuilderTest:
+    '''
+    This class is used to create Continuous Recognition Memory experiments.
+    '''
+
+    def __init__(self, img_size = 10, n_imgs = (2**10)-1, exp_size = (2**10)-1, seq_len = 2, n_initial_view = 1, n_back = 1, p = 0.5, test_split = 0.3):
+        '''
+        Args:
+            img_size: The size of individual "image" elements. int.
+            n_imgs: The total number of "images" to create for the image stack. int.
+            exp_size: The length of the output arrays X and y, i.e., the number of images
+                that the learner will see over the course of the experiment.
+            n_initial_view: The number of images that should be shown before any are 
+                repeated.
+            n_back: The number of time steps back from which we will pull a previously seen image
+                for "seen" images in the experiment. i.e., if n_back=1, every "seen" image will be the same
+                as the image shown 1 time step before.
+            p: The probability of an image in the sequence being seen (as opposed to unseen).
+            test_split: The proportion of experiment images to be allocated to the validation set. 
+                The remainer will go in the training set. For exp_size == 1000 and test_split == 0.3,
+                700 images will be in training set and 300 in test set.
+        '''
+        self.img_size = img_size
+        self.n_imgs = n_imgs
+        self.exp_size = exp_size
+        self.seq_len = seq_len
+        if n_initial_view < n_back:
+            self.n_initial_view = n_back+1
+        else:
+            self.n_initial_view = n_initial_view
+        self.n_back = n_back
+        self.p = p
+        self.test_split = test_split
+
+    def build_stack(self):
+
+        # Ensure that we can create n_imgs unique binary arrays at size img_size 
+        max_int = 2**self.img_size - 1 
+        if max_int < self.n_imgs:
+            raise Exception("img_size is too small given number of n_imgs")
+
+        # Create a list of random integers
+        source_ints = random.sample(range(self.n_imgs), self.n_imgs)
+        source = []
+
+        # Calculate max_length of binary arrays so that we can do zero-padding
+        max_length = len(bin(max_int)) - 2
+
+        # Create list of binary arrays with zero padding
+        for n in source_ints:
+            n_arr = [int(d) for d in bin(n)[2:]]    # Create binary array from integer
+            z = [0] * (max_length - len(n_arr))     # Create leading zeros
+            out = z + n_arr                         # Add leading zeros to binary array
+            source.append(out)
+
+        # Verify there are no duplicates
+        for x in source:
+            count = source.count(x) 
+            if count > 1:
+                raise Exception("There are duplicate entries in image stack")
+
+        return source
+
+    def build_exp(self):
+        '''
+        Returns:
+            X: A list of lists of size img_size, n_imgs containing the experiment "images". 
+            y: A list of integers randomly chosen from the set [0, 1] that server as target
+                labels. 0 means the image is novel and 1 means the image has been shown earlier 
+                in the experiment.
+        '''
+        # Make IMAGE STACK (img_source)
+        # Create a list of lists, where each sublist is a unique combination of
+        # zeros and ones.    
+        img_source = self.build_stack()
+
+        # Make EXPERIMENT TARGETS (y)
+        # Create a random sequence of zeros and ones, also in a list
+        y = np.random.choice([0,1], size = self.exp_size, p = [1-self.p, self.p]).tolist()
+        bound_idx = floor(self.exp_size*(1-self.test_split))
+        y_train = y[:bound_idx]
+        y_test = y[bound_idx:]
+        y_train[0:self.n_initial_view] = [0]*self.n_initial_view # ensure first few images are "unseen" or "new"
+        y_test[0:self.n_initial_view] = [0]*self.n_initial_view # ensure first few images are "unseen" or "new"
+
+        # Make IMAGE LIST for EXPERIMENT (X)
+        # Create a new empty list the same size as the random sequence.
+        # Iterate over the random sequence: for each 0, pop an element from the  
+        # image stack and place it in the corresponding image list entry.
+        X_train = [0] * len(y_train)
+        for idx in range(len(y_train)):
+            if y_train[idx] == 0:
+                # get from img_source
+                X_train[idx] = img_source.pop()
+            else:
+                # get from previously seen images
+                #choice_idx = np.random.choice(idx)
+                choice_idx = idx - self.n_back
+                X_train[idx] = X_train[choice_idx]
+
+        # Break X_train into sequences
+        X_train_seq = []
+        y_train_seq = []
+        for idx in range(len(y_train)-self.seq_len+1):            
+            X_train_seq.append(X_train[idx:idx+self.seq_len])
+            y_train_seq.append(y_train[idx+self.seq_len-1])
+        
+        X_test = [0] * len(y_test)
+        for idx in range(len(y_test)):
+            if y_test[idx] == 0:
+                # get from img_source
+                X_test[idx] = img_source.pop()
+            else:
+                # get from previously seen images
+                #choice_idx = np.random.choice(idx)
+                choice_idx = idx - self.n_back
+                X_test[idx] = X_test[choice_idx]
+        
+        # Break X_test into sequences
+        X_test_seq = []
+        y_test_seq = []
+        for idx in range(len(y_test)-self.seq_len+1):            
+            X_test_seq.append(X_test[idx:idx+self.seq_len])
+            y_test_seq.append(y_test[idx+self.seq_len-1])
+
+        return X_train_seq, y_train_seq, X_test_seq, y_test_seq
---
+++ tests/test_lstm_exp.py
@@ -0,0 +1,219 @@
+'''
+TO DO:
+    - Sort out the role of self.num_layers in h_0 and c_0, I don't think it's right.
+        I think it really only applies to the LSTM stacked layers.
+    - FIgure out, what is a really, truly simple version of this task for an LSTM. Can we do with it just integers instead of
+         "images"?
+
+'''
+
+import gen_exp
+from gen_exp import expBuilderTest
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+from torch.autograd import Variable 
+import numpy as np
+import pickle
+
+# Create experiment
+seq_len = 100
+img_size = 12
+max_n_imgs = 2**img_size - 1
+exp_generator = expBuilderTest(img_size = img_size, n_imgs = max_n_imgs, exp_size = max_n_imgs,
+                                seq_len = seq_len, n_initial_view = 1, n_back = 2, p = 0.5 )
+
+X_train, y_train, X_test, y_test = exp_generator.build_exp()
+
+# Wrap train and test data in PyTorch tensors and Variables
+X_train_tensors = Variable(torch.Tensor(X_train))
+y_train_tensors = Variable(torch.Tensor(y_train))
+X_test_tensors = Variable(torch.Tensor(X_test))
+y_test_tensors = Variable(torch.Tensor(y_test)) 
+
+print("Training Shape", X_train_tensors.shape, y_train_tensors.shape)
+print("Testing Shape", X_test_tensors.shape, y_test_tensors.shape) 
+
+'''
+#reshaping to (len(data), seq_len, input_size)
+X_train_tensors_final = torch.reshape(X_train_tensors, (X_train_tensors.shape[0], X_train_tensors.shape[1], X_train_tensors.shape[1]))
+X_test_tensors_final = torch.reshape(X_test_tensors, (X_test_tensors.shape[0], 1, X_test_tensors.shape[1])) 
+y_train_tensors_final = torch.reshape(y_train_tensors, (y_train_tensors.shape[0], 1))
+y_test_tensors_final = torch.reshape(y_test_tensors, (y_test_tensors.shape[0], 1)) 
+'''
+X_train_tensors_final = X_train_tensors
+X_test_tensors_final = X_test_tensors
+y_train_tensors_final = torch.reshape(y_train_tensors, (y_train_tensors.shape[0], 1))
+y_test_tensors_final = torch.reshape(y_test_tensors, (y_test_tensors.shape[0], 1)) 
+
+print("Training Shape", X_train_tensors_final.shape, y_train_tensors_final.shape)
+print("Testing Shape", X_test_tensors_final.shape, y_test_tensors_final.shape) 
+
+batch_size = 32
+train_dataset = TensorDataset(X_train_tensors_final, y_train_tensors_final)
+train_loader = DataLoader(train_dataset, batch_size=batch_size)
+test_dataset = TensorDataset(X_test_tensors_final, y_test_tensors_final)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+class LSTM1(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super(LSTM1, self).__init__()
+        self.num_classes = num_classes #number of classes
+        self.num_layers = num_layers #number of layers
+        self.input_size = input_size #input size
+        self.hidden_size = hidden_size #hidden state
+        self.seq_length = seq_length #sequence length
+
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                          num_layers=num_layers, batch_first=True) #lstm
+        self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
+        self.relu = nn.ReLU()
+        self.fc = nn.Linear(128, num_classes) #fully connected last layer
+    
+    def forward(self,x):
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
+        # Propagate input through LSTM
+        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
+        #print('hn shape:', hn.shape)
+        #print('output shape:', output.shape)
+        output = output[:, -1, :]
+        #print('output reshaped:', output.shape)
+        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+        #print('hn shape:', hn.shape)
+        out = self.relu(output)
+        #print('relu shape:', out.shape)
+        out = self.fc_1(out) #first Dense
+        #print('fc_1 shape:', out.shape)
+        out = self.relu(out) #relu
+        #print('relu again shape:', out.shape)
+        out = self.fc(out) #Final Output
+        #print('final fc shape:', out.shape)
+        return out
+
+num_epochs = 100
+learning_rate = 0.01 
+gamma = 0.999
+# best performance so far was with learning_rate == 0.01 and gamma == 0.999, 50 hidden units and 1 layer
+# Just over 82.68% test accuracy and 0.27 loss on both train and test
+# Keep making the model bigger, try to overfit training then optimize
+
+# NEVER MIND my training data was in my test_loader :(
+
+input_size = img_size #number of features
+hidden_size = 200 #number of features in hidden state
+num_layers = 1 #number of stacked lstm layers
+# Note: I tried 10 layers and that reeeeeeally slowed down the increase in accuracy
+ 
+num_classes = 1 #number of output classes 
+
+# Instantiate the LSTM
+lstm1 = LSTM1(num_classes, input_size, hidden_size, num_layers, X_train_tensors_final.shape[1]) #our lstm class 
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(lstm1.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=gamma, verbose=True)
+
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]
+    acc = torch.round(acc * 100)
+    
+    return acc
+
+# Function to save the model 
+def saveModel(model): 
+    path = "tests/models/test_lstm_exp.pt" 
+    torch.save(model.state_dict(), path)
+
+best_acc = 0.0
+save_obj = {'epoch' : [],
+            'train loss' : [],
+            'test loss' : [],
+            'train accuracy' : [],
+            'test accuracy' : [],
+            'learning rate' : learning_rate,
+            'gamma' : gamma,
+            'hidden size' : hidden_size,
+            'num_layers' : num_layers}
+for epoch in range(num_epochs):
+    lstm1.train()
+    running_train_loss = 0.0 
+    running_test_loss = 0.0
+    running_train_acc = 0.0 
+    running_test_acc = 0.0 
+    train_n = 0
+    test_n = 0
+    for train_batch, train_labels in train_loader:
+        train_out = lstm1.forward(train_batch) #forward pass
+        optimizer.zero_grad() #calculate the gradient, manually setting to 0
+
+        # Calculate train loss and accuracy
+        train_loss = loss_fn(train_out, train_labels)
+        running_train_loss += train_loss.item()
+        train_n += train_out.size(0) 
+        #train_acc = (torch.round(train_out) == y_train_tensors_final).sum().item()/train_n
+        train_acc = binary_acc(train_out, train_labels)
+        running_train_acc += train_acc.item()*train_batch.size(0)
+        train_loss.backward() # backprop loss
+        optimizer.step() #improve from loss, i.e backprop
+
+    # Calculate test loss and accuracy
+    with torch.no_grad():
+        lstm1.eval()
+        for test_batch, test_labels in test_loader:
+            test_out = lstm1.forward(test_batch)
+            test_loss = loss_fn(test_out, test_labels)
+            running_test_loss += test_loss.item()
+            test_n += test_out.size(0) 
+            test_acc = binary_acc(test_out, test_labels)
+            running_test_acc += test_acc.item()*test_batch.size(0)
+    scheduler.step()
+    
+    # Calculate performance states for epoch
+    train_loss_epoch = running_train_loss/len(train_loader) 
+    test_loss_epoch = running_test_loss/len(test_loader)
+    train_acc_epoch = (running_train_acc)/train_n
+    test_acc_epoch = (running_test_acc)/test_n
+    
+    print("Epoch: %d, train loss: %1.5f, test loss: %1.5f, train acc: %1.5f, test acc: %1.5f" % (epoch, train_loss_epoch, test_loss_epoch, train_acc_epoch, test_acc_epoch)) 
+
+    save_obj['epoch'].append(epoch)
+    save_obj['train loss'].append(train_loss_epoch)
+    save_obj['train accuracy'].append(train_acc_epoch)
+    save_obj['test loss'].append(test_loss_epoch)
+    save_obj['test accuracy'].append(test_acc_epoch)
+
+    if test_acc_epoch > best_acc: 
+        saveModel(lstm1)
+        best_acc = test_acc_epoch 
+
+    if epoch % 100 == 0:
+        save_dict = {}
+        with open('tests/results/test_lstm_exp.pickle', 'wb') as handle:
+            pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+print('\n')
+print('Best accuracy:', best_acc)
+
+'''
+# Print some examples for sanity check!
+with torch.no_grad():
+    lstm1.eval()
+    num_exs = 5
+    for test_batch, test_labels in test_loader:
+        test_out = lstm1.forward(test_batch)
+        y_pred_tag = torch.round(torch.sigmoid(test_out))
+
+        for idx in range(num_exs):
+            print('Last two inputs for sequence:', idx)
+            print(test_batch[idx][-2:])
+            print('Target sequence:', idx)
+            print(y_pred_tag[idx])
+            print('\n')
+
+        break
+'''
+
